ffbc5797b5ef74511e4d4d8252b38312dc49142d
Ali THammoud <ali.hammoud@montymobile.com>
2023-11-27 20:33:10 +0200

    initial commit

---
+++ AppleFrameworks-SwiftUI/AppleFrameworks_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameworks_SwiftUIApp.swift
+//  AppleFrameworks-SwiftUI
+//
+//  Created by Ali Hammoud on 27/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameworks_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
