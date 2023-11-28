d49ff3dcc93083eb298399555a153fa04e79b92b
Ali THammoud <ali.hammoud@montymobile.com>
2023-11-28 16:22:47 +0200

    add grid view

--- AppleFrameworks-SwiftUI/FrameworkGridView.swift
+++ AppleFrameworks-SwiftUI/FrameworkGridView.swift
@@ -8,12 +8,39 @@
 import SwiftUI
 
 struct FrameworkGridView: View {
+    let columns: [GridItem] = [GridItem(.flexible()),
+                               GridItem(.flexible()),
+                               GridItem(.flexible())]
+    
+    var body: some View {
+        
+        NavigationView {
+            ScrollView{
+                LazyVGrid(columns: self.columns) {
+                    ForEach(MockData.frameworks) { frameWork in
+                        FrameworkTitleView(framework: frameWork)
+                    }
+                }
+            }
+            .navigationTitle("🍎 Frameworks")
+        }
+    }
+}
+
+struct FrameworkTitleView: View {
+
+    let framework: Framework
+        
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Image(framework.imageName)
+                .resizable()
+                .frame(width: 90, height: 90)
+            Text(framework.name)
+                .font(.title2)
+                .fontWeight(.semibold)
+                .scaledToFit()
+                .minimumScaleFactor(0.6)
         }
         .padding()
     }
@@ -22,5 +49,6 @@ struct FrameworkGridView: View {
 struct FrameworkGridView_Previews: PreviewProvider {
     static var previews: some View {
         FrameworkGridView()
+            .preferredColorScheme(.dark)
     }
 }
